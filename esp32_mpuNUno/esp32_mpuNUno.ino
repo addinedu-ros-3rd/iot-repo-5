67d6c7218cbb589600f6167f20be17d520a8e150
HongKi Cho <whghdrl9977@gmail.com>
2023-10-26 11:48:41 +0900

    fix: communication code fix

---
+++ esp32_mpuNUno/esp32_mpuNUno.ino
@@ -0,0 +1,71 @@
+#include <Wire.h>
+
+// PIN SETUP
+
+// MPU6050
+// SCL 1 SDA 2
+
+// UNO
+// TX  17
+// RX  18
+
+#define MPU_SDA 2
+#define MPU_SCL 1
+
+
+const int MPU_ADDR = 0x68; // I2C address of the MPU-6050
+int16_t AcX, AcY, AcZ, Tmp, GyX, GyY, GyZ;
+
+void setup() {
+  setupMPU();
+
+  Serial.begin(115200);
+  Serial1.begin(115200);
+  Serial.println("Setup complete");
+}
+
+void loop() {
+
+  printAccGyro();
+
+  readFromUno();
+  
+  delay(100);
+}
+
+void readFromUno() {
+  if (Serial1.available()){
+    String s = Serial1.readStringUntil('\n');
+    Serial.println(s);
+  }
+}
+
+void setupMPU() {
+  Wire.begin(MPU_SDA, MPU_SCL, 100000); // sda, scl
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
+}
+
+void printAccGyro() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU_ADDR, 14, true); // request a total of 14 registers
+  AcX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  AcY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  AcZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  Tmp = Wire.read() << 8 | Wire.read(); // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  GyX = Wire.read() << 8 | Wire.read(); // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  GyY = Wire.read() << 8 | Wire.read(); // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  GyZ = Wire.read() << 8 | Wire.read(); // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+
+
+  Serial.print(AcX); Serial.print(" , ");
+  Serial.print(AcY); Serial.print(" , ");
+  Serial.print(AcZ); Serial.print(" , ");
+  Serial.print(GyX); Serial.print(" , ");
+  Serial.print(GyY); Serial.print(" , ");
+  Serial.print(GyZ); Serial.print("\n");
+}
---
+++ uno_rfid_test/uno_rfid_test.ino
@@ -0,0 +1,79 @@
+#include <SPI.h>
+#include <MFRC522.h>
+
+//PIN SETUP
+//RST   9
+//SDA   10
+//SCK   13
+//MOSI  11
+//MISO  12
+
+#define RST_PIN     9 // any available GPIO          
+#define SS_PIN      10 // SPI_CS -> GPIO 10
+ 
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the class
+MFRC522::MIFARE_Key key;
+
+// Init array that will store new NUID 
+byte nuidPICC[4];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  SPI.begin(); // Init SPI bus
+  rfid.PCD_Init(); // Init MFRC522
+
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  scanRFID();
+  delay(100);
+}
+
+void scanRFID() {
+
+  // Reset the loop if no new card present on the sensor/reader. This saves the entire process when idle.
+  if ( ! rfid.PICC_IsNewCardPresent()){
+    return;
+  }
+  // Verify if the NUID has been readed
+  if ( ! rfid.PICC_ReadCardSerial()) 
+    return;
+
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    return;
+  }
+  
+  // Store NUID into nuidPICC array
+  for (byte i = 0; i < 4; i++) {
+    nuidPICC[i] = rfid.uid.uidByte[i];
+  }
+  
+  Serial.print(F("RFID "));
+  printHex(rfid.uid.uidByte, rfid.uid.size);
+  Serial.println("");
+  
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+
+/**
+ * Helper routine to dump a byte array as hex values to Serial. 
+ */
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}

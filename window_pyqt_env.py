5cd9de3fca5a30b81216f669cc76e35470ce1cbd
HongKi Cho <whghdrl9977@gmail.com>
2023-10-26 13:53:44 +0900

    remove: window_pyqt_env.py (internal testing code)

--- window_pyqt_env.py
+++
@@ -1,6 +0,0 @@
-import os, PyQt5
-
-# 윈도우 pyqt5 설정
-dirname = os.path.dirname(PyQt5.__file__)
-plugin_path = os.path.join(dirname, 'Qt5', 'plugins', 'platforms')
-os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path

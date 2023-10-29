69622198f31ed9f4191eb7fdde95b4476bde7350
raematchaaa <raemoon5349@gmail.com>
2023-10-29 14:44:19 +0900

    refactor: change in features and datatype

--- segwayDB.sql
+++ segwayDB.sql
@@ -1,36 +1,40 @@
 DROP TABLE IF EXISTS `mpu6050`;
 CREATE TABLE mpu6050
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     time_stamp TIMESTAMP NOT NULL,
-    gyro_yaw DECIMAL(11, 5),
-    gyro_pitch DECIMAL(11, 5),
-    gyro_roll DECIMAL(11, 5),
+    acc_x INT,
+    acc_y INT,
+    acc_z INT,
+    gyro_x INT,
+    gyro_y INT,
+    gyro_z INT,
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS `motor`;
 CREATE TABLE motor
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     time_stamp TIMESTAMP NOT NULL,
     direction VARCHAR(2),
     PRIMARY KEY (id)
 );
 
-DROP TABLE IF EXISTS `rfid_user`;
-CREATE TABLE rfid_user
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE user
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     uid VARCHAR(100),
-    user_name VARCHAR(100),
+    name VARCHAR(100),
+    sign_in_date DATE,
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS `rfid_tag_log`;
 CREATE TABLE rfid_tag_log
 (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     time_stamp TIMESTAMP NOT NULL,
     uid VARCHAR(100),
     PRIMARY KEY (id)
